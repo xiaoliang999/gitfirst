fb5e08bae21ca6f9a2e1343ae57de79cd611bf71
xiaoliang999 <17601460206@163.com>
2020-05-23 18:25:55 +0800

    赵露思

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,5 +1,8 @@
 public class HelloGit {
     public static void main(String[] args) {
+
         System.out.println("hello github");
+        System.out.println("hello chenqianqian");
+        System.out.println("hello zhaolusi");
     }
 }
